314bb9ec9a5a84e74d13dc979bc4cca6a2ececaf
Yuumi <69478059+yumyum0035@users.noreply.github.com>
2021-05-06 13:14:02 +0200

    Moved everything
    moved everything to a separate repo and this one will be used for CV purposes

--- modul01/20210217/webpack-starter-typescript-master/src/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var classes_1 = require("src/assets/classes");
-var mensaje = 'Hola Mundo!';
-console.log(mensaje);
-var lobezno = new classes_1.Xmen('Logan', 'inmortal');
--- modul01/20210217/webpack-starter-typescript-master/src/index.ts
+++
@@ -1,5 +0,0 @@
-import {Xmen} from './assets/classes/xmen.class';
-
-const lobezno = new Xmen ('Logan', 'inmortal');
-
-lobezno.imprimir();
