3958aff29d2102e36899f824ff6b3284f14649ce
yumyum0035 <yuumizilla@gmail.com>
2023-04-06 14:18:23 +0200

    placeholder 3d page

--- src/pages/art3D.tsx
+++ src/pages/art3D.tsx
@@ -2,7 +2,12 @@ import React, { useState, useEffect } from "react";
 import "./art3D.css";
 
 const Art3D = () => {
-  return <div>3D art</div>;
+  return (
+    <div>
+      <h2>3D art</h2>
+      <p>placeholder for no as I figure wtf is wrong with git 👍</p>
+    </div>
+  );
 };
 
 export default Art3D;
